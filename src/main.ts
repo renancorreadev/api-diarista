323e3555586f50f5551d3bd0775a2636b1e778b5
renancorreadev <producer@skynance.net>
2023-02-15 09:49:49 -0300

    chore: configure hbs on the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const viewPatch = join(__dirname, '..', 'views');
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(viewPatch);
+  app.setViewEngine('hbs');
+
   await app.listen(3000);
 }
 bootstrap();
