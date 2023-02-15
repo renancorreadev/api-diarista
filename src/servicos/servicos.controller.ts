7d3d5f08f5d7b57b96aa58b61a166d559ffe10b7
renancorreadev <producer@skynance.net>
2023-02-15 17:14:29 -0300

    feat: create view for admin/services

--- src/servicos/servicos.controller.ts
+++ src/servicos/servicos.controller.ts
@@ -1,20 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Render,
+} from '@nestjs/common';
 import { ServicosService } from './servicos.service';
 import { CreateServicoDto } from './dto/create-servico.dto';
 import { UpdateServicoDto } from './dto/update-servico.dto';
 
-@Controller('servicos')
+@Controller('admin/servicos')
 export class ServicosController {
   constructor(private readonly servicosService: ServicosService) {}
+
+  @Get()
+  @Render('servicos/cadastrar')
+  exibirCadastrar() {
+    //
+  }
 
   @Post()
   create(@Body() createServicoDto: CreateServicoDto) {
     return this.servicosService.create(createServicoDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.servicosService.findAll();
   }
 
   @Get(':id')
