f8b7afe146b1572a752c96315d8e016af2ab2b17
renancorreadev <producer@skynance.net>
2023-02-15 09:11:07 -0300

    chore: configure env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmConfigService } from './database/typeorm-config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
     }),
+    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/database/typeorm-config.ts
+++ src/database/typeorm-config.ts
@@ -7,16 +7,16 @@ import { join } from 'path';
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(connectionName?: string): TypeOrmModuleOptions {
     return {
-      database: 'ediaristas',
-      username: 'root',
-      password: 'Astro119',
+      database: process.env.DATABASE_NAME,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
       type: 'mysql',
       synchronize: false,
       entities: [join(__dirname, '..', '**type/*entity.{ts, js}')],
       migrations: [join(__dirname, '..', '**type/*migrations.{ts, js}')],
       namingStrategy: new SnakeNamingStrategy(),
-      host: '127.0.0.1',
-      port: 3306,
     };
   }
 }
