ccfa92ad899456a13298b028863e4c7ec06e6a48
renancorreadev <producer@skynance.net>
2023-02-16 11:05:40 -0300

    feat: create validation to update and create a new service

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 import * as methodOverride from 'method-override';
 import * as exphbs from 'express-handlebars';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const viewPatch = join(__dirname, '..', 'views');
+  app.useGlobalPipes(new ValidationPipe());
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(viewPatch);
